165aee3837676e8eed180abfec396d3a95c913cc
Muhammad-Ali-Asghar-Rizwan <programingmaster22@gmail.com>
2024-12-20 16:24:35 +0500

    update

--- src/app/Arrival/page.tsx
+++ src/app/Arrival/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import ArrivalCard from './pageCard';
-import { FaStar } from "react-icons/fa";
 
 
 const Arrival = () => {
--- src/app/Selling/page.tsx
+++ src/app/Selling/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Link from 'next/link';
-import { FaStar } from "react-icons/fa";
 import SellingCard from './pageCard';
 
 
