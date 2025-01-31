88a0fb9cb302a4e7d4b42a7a6aff894f7f2dc9fd
Muhammad-Ali-Asghar-Rizwan <programingmaster22@gmail.com>
2025-02-01 03:39:14 +0500

    upadte

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,16 +1,8 @@
-// /** @type {import('postcss-load-config').Config} */
-// const config = {
-//   plugins: {
-//     tailwindcss: {},
-//   },
-// };
-
-// export default config;
-
-
-module.exports = {
-  plugins: [
-    'tailwindcss',
-    'autoprefixer',
-  ],
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+  },
 };
+
+export default config;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}', // If you're using Next.js 13 with the app directory
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
