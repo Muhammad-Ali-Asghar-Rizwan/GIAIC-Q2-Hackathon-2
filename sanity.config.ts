0cddbbdcf94aa4cd5c4a31ab3cb65259eb728a44
Muhammad-Ali-Asghar-Rizwan <programingmaster22@gmail.com>
2025-02-05 00:04:45 +0500

    UPdeskStructuredeskStructure/deskStructure

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,31 +1,61 @@
-'use client'
+// 'use client'
 
-/**
- * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
- */
+// /**
+//  * This configuration is used to for the Sanity Studio that’s mounted on the `\src\app\studio\[[...tool]]\page.tsx` route
+//  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-// import {structureTool} from 'sanity/structure'
+// import {visionTool} from '@sanity/vision'
+// import {defineConfig} from 'sanity'
+// // import {structureTool} from 'sanity/structure'
 
-// Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+// // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+// import {apiVersion, dataset, projectId} from './src/sanity/env'
+// import {schema} from './src/sanity/schemaTypes'
+// import {structure} from './src/sanity/structure'
 
+// export default defineConfig({
+//   basePath: '/studio',
+//   projectId : "p98zwfgl",
+//   dataset,
+//   // Add and edit the content schema in the './sanity/schemaTypes' folder
+//   schema,
+//   plugins: [
+//     // structureTool({structure}),
+//     // Vision is for querying with GROQ from inside the Studio
+//     // https://www.sanity.io/docs/the-vision-plugin
+//     visionTool({defaultApiVersion: apiVersion}),
+//   ],
+// })
+
+
+
+
+
+
+
+
+
+
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import schemas from './src/sanity/schemaTypes/schemas';
+import deskStructure from './src/sanity/schemaTypes/deskStructure';
 export default defineConfig({
-  basePath: '/studio',
-  projectId : "p98zwfgl",
-  dataset,
-  // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
+  name: 'default',
+  title: 'Your Project Title',
+
+  projectId: 'p98zwfgl',
+  dataset: 'production',
+
   plugins: [
-    // structureTool({structure}),
-    // Vision is for querying with GROQ from inside the Studio
-    // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    deskTool({
+      structure: deskStructure,
+    }),
+    visionTool(),
   ],
-})
 
-
-
+  schema: {
+    types: schemas,
+  },
+});
