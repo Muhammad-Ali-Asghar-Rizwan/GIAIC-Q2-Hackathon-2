89722d9619513653ff2a0c5500bd8636ff332194
Muhammad-Ali-Asghar-Rizwan <programingmaster22@gmail.com>
2025-02-05 01:19:30 +0500

    UPDATE

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -74,7 +74,8 @@
 
 
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { client } from '../../sanity/lib/client';
+import { client } from '../../../sanity/lib/client';
+// import { client } from '../../sanity/lib/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
